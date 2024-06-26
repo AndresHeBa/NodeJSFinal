4dc5d6312356aed90ff6c865b231c782863cd688
Andres Heredia <herediaandres040@gmail.com>
2024-06-26 11:36:48 -0600

    Router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express"); //importar express 
+const router = express.Router();
 const bodyParser = require("body-parser"); 
 const nodemailer = require('nodemailer');
 const cors = require('cors');
