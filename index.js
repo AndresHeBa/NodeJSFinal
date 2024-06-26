0bf238c63df48043a81e8c2199dc5b5c376e4711
AndresHeBa <124836762+AndresHeBa@users.noreply.github.com>
2024-06-25 23:21:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,23 +26,11 @@ const firebaseConfig = {
   appId: "1:668310300478:web:496f38b2c0a04726842e6f"
 };
 
-const serviceAccount = {
-  'type': 'service_account',
-  'project_id': 'proyecto-final-9469a',
-  'private_key_id': process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  'private_key': (process.env.FIREBASE_ADMIN_PRIVATE_KEY),
-  'client_email': process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  'client_id': process.env.FIREBASE_ADMIN_CLIENT_ID,
-  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-  'token_uri': 'https://oauth2.googleapis.com/token',
-  'auth_provider_x509_cert_url': process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-  'client_x509_cert_url': process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
-};
-
-
 // Initialize Firebase
 
 var admin = require("firebase-admin");
+
+var serviceAccount = require("proyecto-final-9469a-firebase-adminsdk-o1d2x-f4c857b006.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
